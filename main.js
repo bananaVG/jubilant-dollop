452bbd4333d15651e33f03e21087212788626a33
bananaVG <78326537+bananaVG@users.noreply.github.com>
2022-12-19 17:20:48 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+  camera=document.getElementById("camera");
+
+  Webcam.attach('#camera');
+
+
+  function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'; 
+    }
+    );
+}
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qsNqnI3yR/model.json',modelloaded);
+
+function modelloaded(){
+    console.log('modelloaded');
+}
+prediction1="";
+prediction2="";
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The first prediction is "+prediction1;
+    speak_data_2="The second prediction is "+prediction2;
+    var utterthis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterthis);
+
+}
